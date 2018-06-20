bfac6e3be641f733f650fdb45f680abbc24b1a5f
Daniel Shokouhi <dshokouhi@gmail.com>
2018-06-19 20:08:10 -0700

    update import

--- switch/neato.py
+++ switch/neato.py
@@ -9,7 +9,7 @@ import requests
 from datetime import timedelta
 from homeassistant.const import STATE_OFF, STATE_ON
 from homeassistant.helpers.entity import ToggleEntity
-from homeassistant.components.neato import NEATO_ROBOTS, NEATO_LOGIN
+from custom_components.neato import NEATO_ROBOTS, NEATO_LOGIN
 from homeassistant.util import Throttle
 
 _LOGGER = logging.getLogger(__name__)
