586c63dc2cdc351502de717e1334711285272f2e
Daniel Shokouhi <dshokouhi@gmail.com>
2018-06-20 08:36:16 -0700

    Correct versioning

--- neato.py
+++ neato.py
@@ -17,8 +17,8 @@ from homeassistant.util import Throttle
 
 _LOGGER = logging.getLogger(__name__)
 
-REQUIREMENTS = ['https://github.com/dshokouhi/pybotvac/archive/0.0.8.zip'
-                '#pybotvac==0.0.8']
+REQUIREMENTS = ['https://github.com/dshokouhi/pybotvac/archive/0.0.9.zip'
+                '#pybotvac==0.0.9']
 
 DOMAIN = 'neato'
 NEATO_ROBOTS = 'neato_robots'
