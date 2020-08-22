ccb8c3b98a8a7fe8e11afb4c3f770ae2401b3d40
Daniel Shokouhi <dshokouhi@gmail.com>
2020-08-22 15:07:37 -0700

    Attempt to allow multiple accounts

--- neato/config_flow.py
+++ neato/config_flow.py
@@ -34,13 +34,14 @@ class NeatoConfigFlow(config_entries.ConfigFlow, domain=NEATO_DOMAIN):
         """Handle a flow initialized by the user."""
         errors = {}
 
-        if self._async_current_entries():
-            return self.async_abort(reason="already_configured")
+        # if self._async_current_entries():
+        #     return self.async_abort(reason="already_configured")
 
         if user_input is not None:
             self._username = user_input["username"]
             self._password = user_input["password"]
             self._vendor = user_input["vendor"]
+            uniqueId = f"{self._username}-{self._vendor}"
 
             error = await self.hass.async_add_executor_job(
                 self.try_login, self._username, self._password, self._vendor
@@ -48,6 +49,8 @@ class NeatoConfigFlow(config_entries.ConfigFlow, domain=NEATO_DOMAIN):
             if error:
                 errors["base"] = error
             else:
+                await self.async_set_unique_id(uniqueId)
+                self._abort_if_unique_id_configured()
                 return self.async_create_entry(
                     title=user_input[CONF_USERNAME],
                     data=user_input,
@@ -70,12 +73,13 @@ class NeatoConfigFlow(config_entries.ConfigFlow, domain=NEATO_DOMAIN):
     async def async_step_import(self, user_input):
         """Import a config flow from configuration."""
 
-        if self._async_current_entries():
-            return self.async_abort(reason="already_configured")
+        # if self._async_current_entries():
+        #     return self.async_abort(reason="already_configured")
 
         username = user_input[CONF_USERNAME]
         password = user_input[CONF_PASSWORD]
         vendor = user_input[CONF_VENDOR]
+        uniqueId = f"{username}-{vendor}"
 
         error = await self.hass.async_add_executor_job(
             self.try_login, username, password, vendor
@@ -84,6 +88,8 @@ class NeatoConfigFlow(config_entries.ConfigFlow, domain=NEATO_DOMAIN):
             _LOGGER.error(error)
             return self.async_abort(reason=error)
 
+        await self.async_set_unique_id(uniqueId)
+        self._abort_if_unique_id_configured()
         return self.async_create_entry(
             title=f"{username} (from configuration)",
             data={
